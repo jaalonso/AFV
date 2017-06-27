998ab917edcd30475ec9060a128f095b5b558926
Jose A. Alonso <JoseA.Alonso@gmail.com>
2017-06-27 06:43:03 +0200

    Tema 2

--- Temas/T2_Introduccion.thy
+++ Temas/T2_Introduccion.thy
@@ -2,15 +2,13 @@ theory T2_Introduccion
 imports Main
 begin
 
-(* Comentarios *)
-(* =========== *)
+section {* Comentarios *}
 
-(* Nota. Esto es un comentario. *)
+text {* Nota. Esto es un comentario. *}
 
-(* Inferencia de tipo *)
-(* ================== *)
+section {* Inferencia de tipo *}
 
-(* Nota. (term t) muestra el tipo del término t. *)
+text {* Nota. (term t) muestra el tipo del término t. *}
 
 term "True"          (* da "True" :: "bool" *)
 term "False"         (* da "False" :: "bool" *)
@@ -18,24 +16,22 @@ term "true"          (* da "true" :: "'a" *)
 term "True \<and> False"  (* da "True \<and> False" :: "bool" *)
 term "True \<and> false"  (* da "True \<and> false" :: "bool" *)
 
-(* Nota:
+text {* Nota:
   + Para ver el tipo de un término, poner el curso sobre él y pulsar
     Ctrl.
   + Para ver la definición de un término, poner el curso sobre él, 
-    pulsar Ctrl y pulsar el botón izquierdo del ratón. *)
+    pulsar Ctrl y pulsar el botón izquierdo del ratón. *}
 
-(* Evaluación de términos *)
-(* ====================== *)
+section {* Evaluación de términos *}
 
-(* Nota. (value t) muestra el valor del término t. *)
+text {* Nota. (value t) muestra el valor del término t. *}
 
 value "True \<and> False" (* da "False" :: "bool" *)
 value "True \<and> P"     (* da "P" :: "bool" *)
 
-(* Ejemplos de sobrecarga *)
-(* ====================== *)
+section {* Ejemplos de sobrecarga *}
 
-(* Los números y las operaciones aritméticas están sobrecargados. *)
+text {* Los números y las operaciones aritméticas están sobrecargados. *}
 
 term  "2 + 1"         (* da "2 + 1" :: "'a" *)
 value "2 + 1"         (* da "1 + 1 + 1" :: "'a *)
@@ -47,38 +43,38 @@ value "2 - (7::int)"  (* da "- 5" :: "int" *)
 term "2"              (* da "2" :: "'a" *)
 term "op +"           (* da "op +" :: "'a \<Rightarrow> 'a \<Rightarrow> 'a" *)
 
-(* Ejemplos de errores de tipo *)
-(* =========================== *)
+section {* Ejemplos de errores de tipo *}
 
-(* Nota. Al evaluar 
-      term "True + False"
-   da el siguiente mensaje de error
-      Type unification failed: No type arity bool :: plus
-      
-      Type error in application: incompatible operand type
-      
-      Operator:  op + :: ??'a \<Rightarrow> ??'a \<Rightarrow> ??'a
-      Operand:   True :: bool
-
-   Nota. Al evaluar
-      term "2 + True"
-   da el siguiente mensaje de error
-      Type unification failed: No type arity bool :: numeral
-      
-      Type error in application: incompatible operand type
-      
-      Operator:  op + 2 :: ??'a \<Rightarrow> ??'a
-      Operand:   True :: bool
-
-   Nota. Al evaluar 
-      term "True \<and> 2"
-   da el siguiente mensaje de error
-      Type unification failed: No type arity bool :: numeral
-      
-      Type error in application: incompatible operand type
-      
-      Operator:  op \<and> True :: bool \<Rightarrow> bool
-      Operand:   2 :: ??'a
-*)
+text {* 
+  Nota. Al evaluar 
+     term "True + False"
+  da el siguiente mensaje de error
+     Type unification failed: No type arity bool :: plus
+     
+     Type error in application: incompatible operand type
+     
+     Operator:  op + :: ??'a \<Rightarrow> ??'a \<Rightarrow> ??'a
+     Operand:   True :: bool
+  
+  Nota. Al evaluar
+     term "2 + True"
+  da el siguiente mensaje de error
+     Type unification failed: No type arity bool :: numeral
+     
+     Type error in application: incompatible operand type
+     
+     Operator:  op + 2 :: ??'a \<Rightarrow> ??'a
+     Operand:   True :: bool
+  
+  Nota. Al evaluar 
+     term "True \<and> 2"
+  da el siguiente mensaje de error
+     Type unification failed: No type arity bool :: numeral
+     
+     Type error in application: incompatible operand type
+     
+     Operator:  op \<and> True :: bool \<Rightarrow> bool
+     Operand:   2 :: ??'a
+*}
 
 end
