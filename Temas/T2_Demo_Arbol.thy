116542c148261778ff5ad8cb20f241056fdd56e2
Jose A. Alonso <JoseA.Alonso@gmail.com>
2017-06-30 06:36:15 +0200

    Tema 2

--- Temas/T2_Demo_Arbol.thy
+++ Temas/T2_Demo_Arbol.thy
@@ -2,6 +2,8 @@ theory T2_Demo_Arbol
 imports Main
 begin
 
+section {* Definición del tipo de datos de árboles *}
+
 text {* ('a arbol) es el tipo de dato de los śrboles con valores en en
 los nodos. *} 
 datatype 'a arbol = Hoja | Nodo "'a arbol" 'a "'a arbol"
@@ -18,28 +20,50 @@ abbreviation ejArbol1 :: "int arbol"
 where
   "ejArbol1 \<equiv> Nodo Hoja (9::int) (Nodo Hoja 4 Hoja)"
 
+section {* Demostración de propiedades de árboles *}  
+  
 text {* Prop.: Las hojas son distintas de los nodos. *}  
 lemma "Hoja \<noteq> Nodo l x r"
 by simp
 
-fun mirror :: "'a arbol \<Rightarrow> 'a arbol" where
-"mirror Hoja = Hoja" |
-"mirror (Nodo l x r) = Nodo (mirror r) x (mirror l)"
+text {* (espejo t) es la imagen especular del árbol t. Por ejemplo, 
+     espejo (Nodo Hoja (9::int) (Nodo Hoja 4 Hoja))
+             = Nodo (Nodo Hoja 4 Hoja) 9 Hoja
+     espejo (Nodo (Nodo Hoja a Hoja) b t)
+            = Nodo (espejo t) b (Nodo Hoja a Hoja)
+*}
+fun espejo :: "'a arbol \<Rightarrow> 'a arbol" 
+where
+  "espejo Hoja         = Hoja" 
+| "espejo (Nodo i x d) = Nodo (espejo d) x (espejo i)"
 
-value "mirror(Nodo (Nodo Hoja a Hoja) b t)"
+value "espejo (Nodo Hoja (9::int) (Nodo Hoja 4 Hoja))"
+lemma "espejo (Nodo Hoja (9::int) (Nodo Hoja 4 Hoja))
+       = Nodo (Nodo Hoja 4 Hoja) 9 Hoja" by simp
+value "espejo (Nodo (Nodo Hoja a Hoja) b t)"
+lemma "espejo (Nodo (Nodo Hoja a Hoja) b t)
+       = Nodo (espejo t) b (Nodo Hoja a Hoja)" by simp
 
-lemma mirror_mirror[simp]: "mirror(mirror t) = t"
-apply(induction t)
+text {* Prop.: La función espejo es idempotente. *}       
+lemma espejo_espejo [simp]: 
+  "espejo (espejo t) = t"
+apply (induction t)
 apply auto
 done
 
-text{* An example of fun: beyond one equation per constructor *}
+section {* Ejemplo de función recursiva no primitiva recursiva *}
 
-fun sep :: "'a \<Rightarrow> 'a list \<Rightarrow> 'a list" where
-"sep a [] = []" |
-"sep a [x] = [x]" |
-"sep a (x#y#zs) = x # a # sep a (y#zs)"
+text {* (intercala x ys) es la lista obtenida intercalando x entre los
+  elementos de ys. Por ejemplo, 
+     intercala a [x,y,z] = [x, a, y, a, z]" by simp
+*} 
+fun intercala :: "'a \<Rightarrow> 'a list \<Rightarrow> 'a list" 
+where
+  "intercala a []       = []" 
+| "intercala a [x]      = [x]" 
+| "intercala a (x#y#zs) = x # a # intercala a (y#zs)"
 
-value "sep a [x,y,z]"
+value "intercala a [x,y,z]"
+lemma "intercala a [x,y,z] = [x, a, y, a, z]" by simp
 
 end
